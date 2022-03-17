13fb56f19b546624005cf144767ee6195c4fb694
Shiva <shanshiva@outlook.com>
2022-03-17 21:05:37 +0530

    DAO Contract Initialized

---
+++ DAO-Contract/contracts/CryptoDevsDAO.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+// We will add the Interfaces here
+
+contract CryptoDevsDAO is Ownable {
+    // We will write contract code here
+}
